ea9ce74fa4cb11a0bbf0f3a96cd04530343dfcba
GustavoTavares <gustavosttavares@gmail.com>
2019-10-15 14:54:11 +0100

    displays

--- board.pl
+++ board.pl
@@ -1,15 +1,66 @@
-/*board(
-displayBoard([
+/*
+display_game([
+[empty, empty, empty, empty],
+[empty, empty, kingB, empty],
+[empty, kingW, empty, empty],
+[empty, empty, empty, empty]
+]).
+
+display_game([
+[queenB, empty, empty, kingB],
+[empty, empty, empty, empty],
+[empty, empty, bishopW, empty],
+[empty, kingW, empty, empty]
+]).
+
+display_game([
+[empty, empty, towerW, kingB],
+[empty, empty, empty, bishopW],
+[pawnB, empty, empty, empty],
+[kingW, empty, empty, empty]
+]).
+*/
+
+board1(
+[
 [empty, empty, empty, empty],
 [empty, empty, kingB, empty],
 [empty, kingW, empty, empty],
 [empty, empty, empty, empty]
 ]
 ).
-*/
+
+board2(
+[
+[queenB, empty, empty, kingB],
+[empty, empty, empty, empty],
+[empty, empty, bishopW, empty],
+[empty, kingW, empty, empty]
+]
+).
+
+board3(
+[
+[empty, empty, towerW, kingB],
+[empty, empty, empty, bishopW],
+[pawnB, empty, empty, empty],
+[kingW, empty, empty, empty]
+]
+).
+
 translate(empty,S) :- S='  '.
 translate(kingB,S) :- S='kB'.
 translate(kingW,S) :- S='kW'.
+translate(queenB,S) :- S='qB'.
+translate(queenW,S) :- S='qW'.
+translate(bishopB,S) :- S='bB'.
+translate(bishopW,S) :- S='bW'.
+translate(towerB,S) :- S='tB'.
+translate(towerW,S) :- S='tW'.
+translate(horseB,S) :- S='hB'.
+translate(horseW,S) :- S='hW'.
+translate(pawnB,S) :- S='pB'.
+translate(pawnW,S) :- S='pW'.
 
 display_game(X):-
         nl,
