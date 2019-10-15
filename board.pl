cc541a2f1fb6967700d444444002b76254baea74
Daniel Gazola Bradaschia <daniel.bradaschia@gmail.com>
2019-10-15 14:20:26 +0100

    Update board.pl

--- board.pl
+++ board.pl
@@ -1,11 +1,37 @@
-board(
-[
+/*board(
+display_board([
 [empty, empty, empty, empty],
 [empty, empty, kingB, empty],
 [empty, kingW, empty, empty],
 [empty, empty, empty, empty]
 ]
 ).
+*/
+translate(empty,S) :- S='  '.
+translate(kingB,S) :- S='kB'.
+translate(kingW,S) :- S='kW'.
+
+display_board(X):-
+        nl,
+        printMatrix(X, 1).
+
+printMatrix([], 4).
+
+printMatrix([H|T], N):-
+        write('  ');
+        N1 is N+1,
+        write(' | '),
+        printLine(Head),
+        printMatrix(T, N1).
+
+printLine([]).
+
+printLine([H|T]):-
+        symbol(H, S),
+        write(S),
+        write(' | '),
+        printLine(T).
+
 
 printTop:-
         write('_________________________').
@@ -27,4 +53,3 @@ printBoard:-
         printSeparatorLine,nl,
         printSeparatorColumn,nl,
         printSeparatorLine,nl.
-
