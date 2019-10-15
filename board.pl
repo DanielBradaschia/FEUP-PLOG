308aacbfeb3f0efa187903c723e588f6f96fb591
Daniel Gazola Bradaschia <daniel.bradaschia@gmail.com>
2019-10-15 13:46:56 +0100

    empty board

---
+++ board.pl
@@ -0,0 +1,21 @@
+printTop:-
+        write('_________________________').
+
+printSeparatorLine:-
+        write('|_____|_____|_____|_____|').
+
+printSeparatorColumn:-
+        write('|     |     |     |     |').
+
+
+printBoard:-
+        printTop,nl,
+        printSeparatorColumn,nl,
+        printSeparatorLine,nl,
+        printSeparatorColumn,nl,
+        printSeparatorLine,nl,
+        printSeparatorColumn,nl,
+        printSeparatorLine,nl,
+        printSeparatorColumn,nl,
+        printSeparatorLine,nl.
+
