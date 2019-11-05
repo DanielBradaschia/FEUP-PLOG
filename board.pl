90c1089cd7b3e173267274f9ffd479a6f81f2867
Daniel Gazola Bradaschia <daniel.bradaschia@gmail.com>
2019-11-05 16:49:30 +0000

    added movement verification to all pieces

--- board.pl
+++ board.pl
@@ -78,3 +78,118 @@ printSeparatorColumn:-
 
 printSeparatorIndex:-
         write('      a    b    c    d ').
+
+
+/*General Movement*/
+verifyMoveInsideBoard(line, col):-
+        line>=1,
+        line=<4,
+        col>=1,
+        col=<4.
+
+verifyMove({line,col,type,_},{line_end,col_end}):-
+        line\=line_end,
+        verifyMoveType({line,col,type},{line_end,col_end}).
+
+verifyMove({line,col,type,_},{line_end,col_end}):-
+        col\=col_end,
+        verifyMoveType({line,col,type},{line_end,col_end}).
+
+verifyMoveVertHor(line, col, line_end, col_end):-
+        X is col_end-col,
+        Y is line_end-line,
+        validateMoveVertHor(X,Y).
+
+validateMoveVertHor(X,Y):-
+        X=0,Y\=0.
+validateMoveVertHor(X,Y):-
+        X\=0,Y=0.
+
+verifyMoveDiag(line, col, line_end, col_end):-
+        X is col_end-col,
+        Y is line_end-line,
+        AX is abs(X),
+        AY is abs(Y),
+        AX=AY.
+
+/*Types of Movement*/
+verifyMoveType({line,col,k},{line_end,col_end}):-
+        verifyMoveKing(line, col, line_end, col_end).
+
+verifyMoveType({line,col,q},{line_end,col_end}):-
+        verifyMoveQueen(line, col, line_end, col_end).
+
+verifyMoveType({line,col,t},{line_end,col_end}):-
+        verifyMoveTower(line, col, line_end, col_end).
+
+verifyMoveType({line,col,b},{line_end,col_end}):-
+        verifyMoveBishop(line, col, line_end, col_end).
+
+verifyMoveType({line,col,h},{line_end,col_end}):-
+        verifyMoveHorse(line, col, line_end, col_end).
+
+verifyMoveType({line,col,p},{line_end,col_end}):-
+        verifyMovePawn(line, col, line_end, col_end).
+
+/*King Movement*/
+verifyMoveKing(line, col, line_end, col_end):-
+        verifyMoveVertHor(line, col, line_end, col_end),
+        AX is abs(col-col_end),
+        AY is abs(line-line_end),
+        validateKingVertHor(AX,AY).
+
+verifyMoveKing(line, col, line_end, col_end):-
+        verifyMoveDiag(line, col, line_end, col_end),
+        AX is abs(col-col_end),
+        AY is abs(line-line_end),
+        validateKingDiag(AX,AY).
+
+validateKingVertHor(AX,AY):-AX=1,AY=0.
+
+validateKingVertHor(AX,AY):-AX=0,AY=1.
+
+validateKingDiag(AX,AX):-AX=1.
+
+/*Queen Movement*/
+verifyMoveQueen(line, col, line_end, col_end):-
+        verifyMoveVertHor(line, col, line_end, col_end).
+
+verifyMoveQueen(line, col, line_end, col_end):-
+        verifyMoveDiag(line, col, line_end, col_end).
+
+/*Tower Movement*/
+verifyMoveTower(line, col, line_end, col_end):-
+        verifyMoveVertHor(line, col, line_end, col_end).
+
+/*Bishop Movement*/
+verifyMoveBishop(line, col, line_end, col_end):-
+        verifyMoveDiag(line, col, line_end, col_end).
+
+/*Horse Movement*/
+verifyMoveHorse(line, col, line_end, col_end):-
+        AX is abs(col-col_end),
+        AY is abs(line-line_end),
+        validateMoveHorse(AX,AY).
+
+validateMoveHorse(AX,AY):-AX=1,AY=2.
+
+validateMoveHorse(AX,AY):-AX=2,AY=1.
+
+/*Pawn Movement*/
+verifyMovePawn(line, col, line_end, col_end):-
+        verifyMoveVertHor(line, col, line_end, col_end),
+        AX is col_end-col,
+        AY is line_end-line,
+        validatePawnVertHor(AX,AY).
+
+verifyMovePawn(line, col, line_end, col_end):-
+        verifyMoveDiag(line, col, line_end, col_end),
+        AX is col_end-col,
+        AY is line_end-line,
+        validatePawnDiag(AX,AY).
+
+validatePawnVertHor(AX,AY):-AX=1,AY=0.
+
+validatePawnDiag(AX,AX):-AX=1.
+
+
