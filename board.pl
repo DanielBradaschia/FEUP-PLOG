98ac936991332590106741acc33d169a3285fd93
Daniel Gazola Bradaschia <daniel.bradaschia@gmail.com>
2019-11-14 17:47:58 +0000

    cvc

--- board.pl
+++ board.pl
@@ -7,7 +7,95 @@ clearScreen:-
 display_game(TAB, PLAYER, win):-
 	printBoard(TAB),
 	nl , print(PLAYER), 
-	write(' : WIN').
+	write(' : WIN'),
+        play.
+
+display_game(TAB, PLAYER, cvc):-
+        (
+                
+                isTowerOnBoard(PLAYER, TAB)
+                ->
+                        repeat,
+                        printBoard(TAB),
+                        
+                        nl, write(' PLAYER '), write(PLAYER),nl,
+                        random(1,4,Action),
+                        gameChoice(Action, TAB, PLAYER, cvc)
+                ;
+                repeat,
+                printBoard(TAB),
+                
+                nl, write(' PLAYER '), write(PLAYER),nl,
+                random(1,3,Action),
+                gameChoice(Action, TAB, PLAYER, cvc)
+        ).
+
+gameChoice(1, TAB, black, cvc):-
+        placePieceAuto(TAB, black, NEWTAB),
+        game_over(NEWTAB, WIN, black, cvc).
+
+gameChoice(1, TAB, white, cvc):-
+        placePieceAuto(TAB, white, NEWTAB),
+        game_over(NEWTAB, WIN, white, cvc).
+
+
+placePieceAuto(TAB, PLAYER, NEWTAB):-
+        random(1,7, Action),
+        translate(Action,PLAYER,TYPE),
+	verifyPlaceAuto(TAB,{LINE,COL,TYPE,PLAYER}),
+        putPieceAuto(TYPE, TAB, PLAYER, NEWTAB).
+
+verifyPlaceAuto(TAB, {LINE, COL, TYPE, PLAYER}):-
+	getPiecesI(TAB, PLIST),
+	nonmember({_,_,TYPE, PLAYER},PLIST).
+
+verifyPlaceAuto(TAB,{LINE,COL,TYPE,PLAYER}):-
+	placePieceAuto(TAB,PLAYER,NEWTAB).
+
+putPieceAuto(TYPE, TAB, PLAYER, NEWTAB):-
+        random(1,5, LINE),
+        random(1,5, COL),
+        verifyMoveInsideBoard(LINE, COL),
+        verifyNotPiece(TAB, {LINE, COL, TYPE, PLAYER}),
+        verifyKingDist(TAB, {LINE, COL, TYPE, PLAYER}),
+        replace(TAB, LINE, COL, {LINE, COL, TYPE, PLAYER}, NEWTAB).
+
+putPieceAuto(TYPE, TAB, PLAYER, NEWTAB):-
+        putPieceAuto(TYPE, TAB, PLAYER, NEWTAB).
+
+gameChoice(2, TAB, black, cvc):-
+        choosePieceAuto(TAB, {LINE, COL, TYPE, black}),
+        movePieceAuto(TAB, {LINE, COL, TYPE, PLAYER}, NEWTAB),
+	game_over(NEWTAB, WIN, black, cvc).
+
+choosePieceAuto(TAB, {LINE, COL, TYPE, black}):-
+        random(1,5, LINE),
+        random(1,5, COL),        
+        verifyPosition(TAB, {LINE, COL, TYPE, PLAYER}).
+
+choosePieceAuto(TAB, {LINE, COL, TYPE, black}):-
+        choosePieceAuto(TAB, {LINE, COL, TYPE, PLAYER}).
+
+movePieceAuto(TAB, {LINE, COL, TYPE, PLAYER}, NEWTAB):-
+        random(1,5, LINEEND),
+        random(1,5, COLEND),        
+        verifyEndPosition(TAB,LINEEND,COLEND,{LINE, COL, TYPE, PLAYER},NEWTAB).
+
+gameChoice(3, TAB, white, cvc):-
+        getWhiteTowerPos({TL,TC, towerW,white}, TAB),
+        getWhiteKingPos({KL,KC, kingW,white}, TAB),
+        replace(TAB, TL, TC, {TL, TC, kingW, white}, NEWTAB),
+        replace(NEWTAB, KL, KC, {KL, KC, towerW, white}, NEWTAB2),
+        display_game(NEWTAB2, black, cvc).
+
+gameChoice(3, TAB, black, cvc):-
+        getBlackTowerPos({TL,TC, towerB,black}, TAB),
+        getBlackKingPos({KL,KC, kingB,black}, TAB),
+        replace(TAB, TL, TC, {TL, TC, kingB, black}, NEWTAB),
+        replace(NEWTAB, KL, KC, {KL, KC, towerB, black}, NEWTAB2),
+        display_game(NEWTAB2, white, cvc).
+
+
 
 display_game(TAB, PLAYER, pvp):-
         (
@@ -16,6 +104,7 @@ display_game(TAB, PLAYER, pvp):-
                 ->
                         repeat,
                         printBoard(TAB),
+                        
                         nl, write(' PLAYER '), write(PLAYER),write(' - '),
                         write('Choose an option!'), nl,
                         write('1. Put Piece on Board'), nl,
@@ -24,10 +113,11 @@ display_game(TAB, PLAYER, pvp):-
                         read(Action),
                         Action > 0,
                         Action < 4,
-                        gameChoice(Action, TAB, PLAYER)
+                        gameChoice(Action, TAB, PLAYER, pvp)
                 ;
                 repeat,
                 printBoard(TAB),
+                
                 nl, write(' PLAYER '), write(PLAYER),write(' - '),
                 write('Choose an option!'), nl,
                 write('1. Put Piece on Board'), nl,
@@ -35,18 +125,18 @@ display_game(TAB, PLAYER, pvp):-
                 read(Action),
                 Action > 0,
                 Action < 3,
-                gameChoice(Action, TAB, PLAYER)
+                gameChoice(Action, TAB, PLAYER, pvp)
         ).
         
 
-gameChoice(3, TAB, white):-
+gameChoice(3, TAB, white, pvp):-
         getWhiteTowerPos({TL,TC, towerW,white}, TAB),
         getWhiteKingPos({KL,KC, kingW,white}, TAB),
         replace(TAB, TL, TC, {TL, TC, kingW, white}, NEWTAB),
         replace(NEWTAB, KL, KC, {KL, KC, towerW, white}, NEWTAB2),
         display_game(NEWTAB2, black, pvp).
 
-gameChoice(3, TAB, black):-
+gameChoice(3, TAB, black, pvp):-
         getBlackTowerPos({TL,TC, towerB,black}, TAB),
         getBlackKingPos({KL,KC, kingB,black}, TAB),
         replace(TAB, TL, TC, {TL, TC, kingB, black}, NEWTAB),
@@ -72,13 +162,13 @@ getBlackTowerPos({PL,PC,towerB,black}, Xss) :-
    member({PL,PC,towerB,black}, Xs).
 
 
-gameChoice(1, TAB, black):-
+gameChoice(1, TAB, black, pvp):-
 	placePiece(TAB, black, NEWTAB),
-	blackWin(NEWTAB, WIN).
+	game_over(NEWTAB, WIN, black, pvp).
 
-gameChoice(1, TAB, white):-
+gameChoice(1, TAB, white, pvp):-
 	placePiece(TAB, white, NEWTAB),
-	whiteWin(NEWTAB, WIN).
+	game_over(NEWTAB, WIN, white, pvp).
 
 placePiece(TAB, PLAYER, NEWTAB):-
         write('Choose an option!'), nl,
@@ -87,9 +177,11 @@ placePiece(TAB, PLAYER, NEWTAB):-
         write('3. Tower'), nl,
         write('4. Horse'), nl,
         write('5. Pawn'), nl,
+        write('6. Return'), nl,
         read(Action),
         Action > 0,
-        Action < 6,
+        Action < 7,
+        Action =:= 6 -> display_game(TAB, PLAYER, pvp);
         translate(Action,PLAYER,TYPE),
 	verifyPlace(TAB,{LINE,COL,TYPE,PLAYER}),
         putPiece(TYPE, TAB, PLAYER, NEWTAB).
@@ -117,15 +209,15 @@ putPiece(TYPE, TAB, PLAYER, NEWTAB):-
         nl, write('Invalid Position. Try again!'), nl,
         putPiece(TYPE, TAB, PLAYER, NEWTAB).
 
-gameChoice(2, TAB, black):-
+gameChoice(2, TAB, black, pvp):-
         choosePiece(TAB, {LINE, COL, TYPE, black}),
         movePiece(TAB, {LINE, COL, TYPE, PLAYER}, NEWTAB),
-	blackWin(NEWTAB, WIN).
+	game_over(NEWTAB, WIN, black, pvp).
 
-gameChoice(2, TAB, white):-
+gameChoice(2, TAB, white, pvp):-
         choosePiece(TAB, {LINE, COL, TYPE, white}),
         movePiece(TAB, {LINE, COL, TYPE, PLAYER}, NEWTAB),
-	whiteWin(NEWTAB, WIN).
+	game_over(NEWTAB, WIN, white, pvp).
 
 verifyKingDist(TAB, {LINE, COL, TYPE, white}):-
         getBlackKingPos({PL,PC,kingB,black}, TAB),
@@ -141,59 +233,42 @@ verifyKingDist(TAB, {LINE, COL, TYPE, black}):-
         AUX is AUXLINE+AUXCOL,
         AUX>=2.
 
-blackWin(TAB, WIN):-
+game_over(TAB, WIN, PLAYER, STATE):-
         getWhiteKingPos({PL,PC,kingW,white}, TAB),
         FRONT is PL-1,
-        \+verifyEndPosition(TAB,FRONT,PC,{PL, PC, kingW,white},NEWTAB),
+        \+verifyVictory(TAB,FRONT,PC,{PL, PC, kingW,white},NEWTAB),
         BACK is PL+1,
-        \+verifyEndPosition(TAB,BACK,PC,{PL, PC, kingW,white},NEWTAB),
+        \+verifyVictory(TAB,BACK,PC,{PL, PC, kingW,white},NEWTAB),
         RIGHT is PC+1,
-        \+verifyEndPosition(TAB,PL,RIGHT,{PL, PC, kingW,white},NEWTAB),
+        \+verifyVictory(TAB,PL,RIGHT,{PL, PC, kingW,white},NEWTAB),
         LEFT is PC-1,
-        \+verifyEndPosition(TAB,PL,LEFT,{PL, PC, kingW,white},NEWTAB),
+        \+verifyVictory(TAB,PL,LEFT,{PL, PC, kingW,white},NEWTAB),
 	display_game(TAB, black, win).
 
-blackWin(TAB, WIN):-
+game_over(TAB, WIN, PLAYER, STATE):-
         getBlackKingPos({PL,PC,kingB,black}, TAB),
         FRONT is PL-1,
-        \+verifyEndPosition(TAB,FRONT,PC,{PL,PC,kingB,black},NEWTAB),
+        \+verifyVictory(TAB,FRONT,PC,{PL,PC,kingB,black},NEWTAB),
         BACK is PL+1,
-        \+verifyEndPosition(TAB,BACK,PC,{PL,PC,kingB,black},NEWTAB),
+        \+verifyVictory(TAB,BACK,PC,{PL,PC,kingB,black},NEWTAB),
         RIGHT is PC+1,
-        \+verifyEndPosition(TAB,PL,RIGHT,{PL,PC,kingB,black},NEWTAB),
+        \+verifyVictory(TAB,PL,RIGHT,{PL,PC,kingB,black},NEWTAB),
         LEFT is PC-1,
-        \+verifyEndPosition(TAB,PL,LEFT,{PL,PC,kingB,black},NEWTAB),
+        \+verifyVictory(TAB,PL,LEFT,{PL,PC,kingB,black},NEWTAB),
 	display_game(TAB, white, win).
 
-blackWin(TAB, WIN):-
+game_over(TAB, WIN, black, pvp):-
 	display_game(TAB, white, pvp).
 
-whiteWin(TAB, WIN):-
-        getBlackKingPos({PL,PC,kingB,black}, TAB),
-        FRONT is PL-1,
-        \+verifyEndPosition(TAB,FRONT,PC,{PL,PC,kingB,black},NEWTAB),
-        BACK is PL+1,
-        \+verifyEndPosition(TAB,BACK,PC,{PL,PC,kingB,black},NEWTAB),
-        RIGHT is PC+1,
-        \+verifyEndPosition(TAB,PL,RIGHT,{PL,PC,kingB,black},NEWTAB),
-        LEFT is PC-1,
-        \+verifyEndPosition(TAB,PL,LEFT,{PL,PC,kingB,black},NEWTAB),
-	display_game(TAB, white, win).
-
-whiteWin(TAB, WIN):-
-        getWhiteKingPos({PL,PC,kingW,white}, TAB),
-        FRONT is PL-1,
-        \+verifyEndPosition(TAB,FRONT,PC,{PL, PC, kingW,white},NEWTAB),
-        BACK is PL+1,
-        \+verifyEndPosition(TAB,BACK,PC,{PL, PC, kingW,white},NEWTAB),
-        RIGHT is PC+1,
-        \+verifyEndPosition(TAB,PL,RIGHT,{PL, PC, kingW,white},NEWTAB),
-        LEFT is PC-1,
-        \+verifyEndPosition(TAB,PL,LEFT,{PL, PC, kingW,white},NEWTAB),
-	display_game(TAB, black, win).
-
-whiteWin(TAB, WIN):-
+game_over(TAB, WIN, white, pvp):-
 	display_game(TAB, black, pvp).
+
+game_over(TAB, WIN, black, cvc):-
+	display_game(TAB, white, cvc).
+
+game_over(TAB, WIN, white, cvc):-
+	display_game(TAB, black, cvc).
+
 
 getWhiteKingPos({PL,PC,kingW,white}, Xss) :-
    member(Xs, Xss),
@@ -204,7 +279,7 @@ getBlackKingPos({PL,PC,kingB,black}, Xss) :-
    member({PL,PC,kingB,black}, Xs).
 
 movePiece(TAB, {LINE, COL, TYPE, PLAYER}, NEWTAB):-
-        write(' Select Destination (Row/Column): '),
+        write(' Select Destination (Row/Column) or 0/0 to Return: '),
         read(LINEEND/COLEND),
         verifyEndPosition(TAB,LINEEND,COLEND,{LINE, COL, TYPE, PLAYER},NEWTAB).
 
@@ -213,6 +288,11 @@ verifyEndPosition(TAB,LINEEND,COLEND,{LINE, COL, TYPE, PLAYER},NEWTAB):-
         verifyNotPiece(TAB, {LINEEND, COLEND, TYPE, PLAYER}),
         verifyMove({LINE, COL, TYPE, PLAYER}, {LINEEND,COLEND}),
         executeMove({LINE, COL, TYPE, PLAYER}, {LINEEND,COLEND}, TAB, NEWTAB).
+
+verifyVictory(TAB,LINEEND,COLEND,{LINE, COL, TYPE, PLAYER},NEWTAB):-
+        verifyMoveInsideBoard(LINEEND, COLEND),
+        verifyNotPiece(TAB, {LINEEND, COLEND, TYPE, PLAYER}),
+        verifyMove({LINE, COL, TYPE, PLAYER}, {LINEEND,COLEND}).
 
 executeMove({LINE, COL, TYPE, PLAYER}, {LINEEND,COLEND}, TAB, NEWTAB):-
         moveAux({LINE, COL, TYPE, PLAYER}, {LINEEND,COLEND}, TAB, NEWTAB2),
@@ -260,7 +340,7 @@ erasePieceSet({LINE, COL, TYPE, PLAYER}, {LPOS, CPOS, TYPE, PLAYER},{LPOS,CPOS,n
 erasePieceSet(_, H, H).
 
 
-gameChoice(_, TAB, PLAYER):-
+gameChoice(_, TAB, PLAYER, pvp):-
         write('Choose a valid option!').
         
 choosePiece(TAB, {LINE, COL, TYPE, PLAYER}):-
@@ -529,7 +609,8 @@ gameMode(2):-
         write('Em desenvolvimento').
 
 gameMode(3):-
-        write('Em desenvolvimento').
+        board(TAB),
+        display_game(TAB, white, cvc).
 
 gameMode(4):-
         nl, nl, nl,
