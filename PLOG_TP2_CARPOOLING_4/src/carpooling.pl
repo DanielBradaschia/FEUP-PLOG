e36c04e7244a76409b023bb10171a6bfd578b9ce
GustavoTavares <gustavosttavares@gmail.com>
2020-01-05 20:01:10 +0000

    update carpooling

--- PLOG_TP2_CARPOOLING_4/src/carpooling.pl
+++ PLOG_TP2_CARPOOLING_4/src/carpooling.pl
@@ -1,137 +1,174 @@
-:-use_module(library(clpfd)).
+:- use_module(library(clpfd)).
+:- use_module(library(lists)).
 
 /*nome | tem carro? (0,1) | deseja levar carro? (0,1) | grupo desejado (id) | grupo indesejado (id)*/
 list(
 [
-[1,  0, 0, 1, 2],
-[2,  1, 1, 2, 3],
-[3,  1, 0, 3, 4],
-[4,  0, 0, 4, 5],
-[5,  1, 1, 5, 1],
-[6,  1, 0, 1, 2],
-[7,  1, 1, 1, 2],
-[8,  0, 0, 3, 4],
-[9,  0, 0, 2, 3],
-[10, 0, 0, 4, 5],
-[11, 1, 1, 4, 1],
-[12, 0, 0, 2, 3],
-[13, 1, 0, 2, 3],
-[14, 0, 0, 3, 4],
-[15, 0, 0, 1, 2],
-[16, 0, 0, 4, 5],
-[17, 1, 1, 3, 4],
-[18, 0, 0, 5, 1],
-[19, 1, 1, 2, 3],
-[20, 0, 0, 3, 4],
-[21, 0, 0, 4, 5],
-[22, 0, 0, 1, 2]
+{1,  0, 0, 1, 2},
+{2,  1, 1, 2, 3},
+{3,  1, 0, 3, 4},
+{4,  0, 0, 4, 5},
+{5,  1, 1, 5, 1},
+{6,  1, 0, 1, 2},
+{7,  1, 1, 1, 2},
+{8,  0, 0, 3, 4},
+{9,  0, 0, 2, 3},
+{10, 0, 0, 4, 5},
+{11, 1, 1, 4, 1},
+{12, 0, 0, 2, 3},
+{13, 1, 0, 2, 3},
+{14, 0, 0, 3, 4},
+{15, 0, 0, 1, 2},
+{16, 0, 0, 4, 5},
+{17, 1, 1, 3, 4},
+{18, 0, 0, 5, 1},
+{19, 1, 1, 2, 3},
+{20, 0, 0, 3, 4},
+{21, 0, 0, 4, 5},
+{22, 0, 0, 1, 2}
 ]
 ).
 
 carpooling(L):-
     list(X),
-    solve(X,N,L), !,
-    write('Total Cars: '),print(N).
+    solve(X,N,L),
+    write('Total Cars: '),print(N),nl.
 
 solve(INPUT,NUMBER,OUTPUT):-
     length(INPUT, NAUX),
     NUMBER is ceiling(NAUX/5),
-    getDrivers(INPUT,NUMBER,DL),
-    length(DL,I2),
-    (I2 #= N ->
-        addWanted(INPUT,DL,OUTPUT)
-    ;
-        N2 is N-I2,
-        getDriversExtra(INPUT,N2,DL1),
-        append(DL,DL1,DLF),
-        addWanted(INPUT,DLF,OUTPUT)
-    ).
+    getDrivers(INPUT,NUMBER,OUTPUT1),
+    length(OUTPUT1,N2),
+    NE is NUMBER-N2,
+    getDriversExtra(INPUT,NE,OUTPUT2),
+    append(OUTPUT1,OUTPUT2,OUTPUT1),
+    write('Drivers: '),print(OUTPUT1),nl,
+    addWanted(INPUT,OUTPUT1,OUTPUT4),
+    addnoUnwanted(INPUT,OUTPUT4,OUTPUT).
 
-/*adiciona wanteds de cada driver*/
+/*add others in car*/
+addnoUnwanted(_,[],_).
+
+addnoUnwanted(X,[H|T],L):-
+    length(H,C),
+    C2 is 5-C,
+    addPeople(X,H,C2,W),
+    append(H,W,CAR),
+    addnoUnwanted(X,T,LAUX),
+    append(LAUX,[CAR],L).
+
+addPeople(_,_,0,[]).
+
+addPeople(X,[HS|_],C,CAR):-
+    getUnwantedGroup(X,HS,UG),
+    getWantedGroup(X,HS,WG),
+    getNeutralIds(X,UG,WG,C,CAR).
+
+/*add wanted people in car*/
 addWanted(_,[],_).
 
 addWanted(X,[H|T],L):-
     getWantedGroup(X,H,G),
     getWantedIds(X,G,W),
-    nonmember(W, [H|T]),
-    append([H],W,CAR),
-    all_distinct(CAR),
+    makeCar(H,W,CAR),
     addWanted(X,T,LAUX),
     append(LAUX,[CAR],L).
 
-/*getWanted Ids*/
-getWantedIds([],_,_).
 
-getWantedIds([H|T], G, L):-
-    getWantedIds(T,G,LAUX),
-    match(H,3,N),
-    checkId(H,N,G,I),
-    append(LAUX,I,L).
+/*add wanted people*/
+addWanted(_,[],_).
 
-checkId(H,G,G,[I]):-
-    match(H,0,I).
+addWanted(X,[H|T],L):-
+    getWantedGroup(X,H,G),
+    getWantedIds(X,G,W),
+    makeCar(H,W,CAR),
+    addWanted(X,T,LAUX),
+    append(LAUX,[CAR],L).
 
-checkId(_,_,_,[]).
+/*create a car with no duplicates*/
+makeCar(H,W,CAR):-
+    member(H,W),
+    deleteH(H,W,W2),
+    append([H],W2,CAR).
+
+makeCar(H,W,CAR):-
+    append([H],W,CAR).
+
+deleteH(_,[],_).
+
+deleteH(H,[H|T],W):-
+    deleteH(H,T,W).
+
+deleteH(H,[HW|T],W):-
+    deleteH(H,T,WAUX),
+    append(WAUX,[HW],W).
 
 /*getWanted Group*/
 getWantedGroup([],_,_).
 
-getWantedGroup([H|T], G, L):-
-    match(H,0,N),
-    checkGroup(H,N,G,L),
-    getWantedGroup(T,G,L).
+getWantedGroup([{ID,_,_,G,_}|_], ID, G).
 
-checkGroup(H,G,G,L):- 
-    match(H,3,L).
+getWantedGroup([_|T],ID,G):-
+    getWantedGroup(T,ID,G).
 
-checkGroup(_,_,_,_).
+/*getUnwanted Group*/
+getUnwantedGroup([],_,_).
 
-/*pessoas que querem levar o carro*/
+getUnwantedGroup([{ID,_,_,_,G}|_], ID, G).
+
+getUnwantedGroup([_|T],ID,G):-
+    getWantedGroup(T,ID,G).
+
+/*getWanted Ids*/
+getWantedIds([],_,_).
+
+getWantedIds([{ID,_,_,G,_}|T], G, L):-
+    getWantedIds(T,G,LAUX),
+    append(LAUX,[ID],L).
+
+getWantedIds([_|T], G, L):-
+    getWantedIds(T,G,L).
+
+
+/*getNeutral Ids*/
+getNeutralIds(_,_,_,0,_).
+
+getNeutralIds([],_,_,_,_).
+
+getNeutralIds([{_,_,_,_,UG}|T], UG, WG, C, L):-
+    getNeutralIds(T,UG,WG,C,L).
+
+getNeutralIds([{_,_,_,WG,_}|T], UG, WG, C, L):-
+    getNeutralIds(T,UG,WG,C,L).
+
+getNeutralIds([{ID,_,_,_,_}|T], UG, WG, C, L):-
+    C2 is C-1,
+    getNeutralIds(T,UG,WG,C2,LAUX),
+    append(LAUX,[ID],L).
+
+/*get drivers who want to bring their car*/
 getDrivers([],_,_).
 
 getDrivers(_,0,_).
 
-getDrivers([H|T],N,DL):-
-    match(H, 1, AUX1),
-    match(H, 2, AUX2),
-    match(H, 0, AUX3),
-    checkDriver(N,N2,AUX3,AUX1,AUX2,D),
-    getDrivers(T,N2,DAUX),
-    append(DAUX,D,DL).
-
-checkDriver(N,N2,AUX3,1,1,[AUX3]):-
+getDrivers([{H,1,1,_,_}|T],N,DL):-
     N2 is N-1,
-    write('Driver : '),
-    write(AUX3),nl.
+    getDrivers(T,N2,DAUX),
+    append(DAUX,[H],DL).
 
-checkDriver(N,N2,_,_,_,[]):-
-    N2 is N.
+getDrivers([_|T],N,DL):-
+    getDrivers(T,N,DL).
 
-/*pessoas que não querem levar o carro, mas tem*/
+
+/*get extra drivers, those who don't want to bring their car*/
 getDriversExtra([],_,_).
 
-getDriversExtra(_,0,_).
+getDriversExtra(_,0,[]).
 
-getDriversExtra([H|T],N,DL):-
-    match(H, 1, AUX1),
-    match(H, 2, AUX2),
-    match(H, 0, AUX3),
-    checkDriverExtra(N,N2,AUX3,AUX1,AUX2,D),
-    getDriversExtra(T,N2,DAUX),
-    append(DAUX,D,DL).
-
-checkDriverExtra(N,N2,AUX3,1,0,[AUX3]):-
+getDriversExtra([{H,1,0,_,_}|T],N,DL):-
     N2 is N-1,
-    write('Driver : '),
-    write(AUX3),nl.
+    getDriversExtra(T,N2,DAUX),
+    append(DAUX,[H],DL).
 
-checkDriverExtra(N,N2,_,_,_,[]):-
-    N2 is N.
-
-/*Get nth element*/
-match([H|_],0,H).
-
-match([_|T],N,H) :-
-    N > 0,
-    N1 is N-1,
-    match(T,N1,H).
+getDriversExtra([_|T],N,DL):-
+    getDriversExtra(T,N,DL).
